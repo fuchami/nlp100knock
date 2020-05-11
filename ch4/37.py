5dc1566169e072d075843d269504e3de465d663f
fuchami <famichiki.yuuki@gmail.com>
2020-05-12 01:29:06 +0900

    chapter4 finished

--- ch4/37.py
+++ ch4/37.py
@@ -4,25 +4,30 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'AppleGothic'
 
+# 先にfilterで絞ってから辞書に入れる処理にしたほうがスマートでした
 def countWord(block_list):
     wordCounts = {}
     for block in block_list:
-        for b in block:
-            word = b['base'] + "_" + b['pos'] + "_" + b['pos1']
-            if word in wordCounts:
-                wordCounts[word] += 1
-            else:
-                wordCounts[word] = 1
+        flg = list(filter(lambda x: x['surface'] == '猫', block))
+        if len(flg) > 0:
+            for b in block:
+                if b['surface'] != '猫':
+                    word = b['surface']
+                    if word in wordCounts:
+                        wordCounts[word] += 1
+                    else:
+                        wordCounts[word] = 1
+    
     return wordCounts
 
 block_list = readFile()
 wordCounts = countWord(block_list)
-# valueで降順にソート
 wordCounts = sorted(wordCounts.items(), key=lambda x: x[1], reverse=True)[:10]
 
 labels = [w[0] for w in wordCounts]
 values = [w[1] for w in wordCounts]
 target = list(zip(*wordCounts))
+
 print(target)
 plt.barh(*target)
 plt.show()
--- ch4/38-39.py
+++ ch4/38-39.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 from readMecab30 import readFile
 import matplotlib.pyplot as plt
+import math
 
 plt.rcParams['font.family'] = 'AppleGothic'
 
@@ -8,7 +9,7 @@ def countWord(block_list):
     wordCounts = {}
     for block in block_list:
         for b in block:
-            word = b['base'] + "_" + b['pos'] + "_" + b['pos1']
+            word = b['surface']
             if word in wordCounts:
                 wordCounts[word] += 1
             else:
@@ -17,12 +18,17 @@ def countWord(block_list):
 
 block_list = readFile()
 wordCounts = countWord(block_list)
-# valueで降順にソート
-wordCounts = sorted(wordCounts.items(), key=lambda x: x[1], reverse=True)[:10]
 
-labels = [w[0] for w in wordCounts]
-values = [w[1] for w in wordCounts]
-target = list(zip(*wordCounts))
-print(target)
-plt.barh(*target)
+# 38.Histogram
+ans = wordCounts.values()
+plt.hist(ans, range=(1,20))
+plt.show()
+
+# 39. Zipfの法則 両対数グラフ
+ans = sorted(wordCounts.items(), key=lambda x: x[1], reverse=True)
+ranks = [math.log(r+1) for r in range(len(ans))]
+values = [math.log(a[1]) for a in ans]
+
+plt.figure(figsize=(8,8))
+plt.scatter(ranks, values)
 plt.show()
